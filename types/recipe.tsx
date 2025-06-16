cba1687fbd1b30b49224da27c5c05620eb9dfde1
addomingo <addomingo10@up.edu.ph>
2025-06-16 18:10:10 +0800

    feat: create recipes data and type

---
+++ types/recipe.tsx
@@ -0,0 +1,9 @@
+interface Recipe {
+    id: string;
+    name: string;
+    ingredients: string[];
+    instructions: string[];
+    cookingTime: string;
+    servings: number;
+    image: string;
+}
